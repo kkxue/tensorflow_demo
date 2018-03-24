b246a0519e7d64c2f48b8d9280506977dd7ba6b9
xueguanwen <xgw1989@sina.com>
2018-03-24 23:23:54 +0800

    Update input_data.py
    fix "TypeError: only integer scalar arrays can be converted to a scalar index"

--- input_data.py
+++ input_data.py
@@ -22,7 +22,7 @@ def maybe_download(filename, work_directory):
 
 def _read32(bytestream):
     dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-    return numpy.frombuffer(bytestream.read(4), dtype=dt)
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
 
 
 def extract_images(filename):
@@ -110,10 +110,10 @@ class DataSet(object):
     def next_batch(self, batch_size, fake_data=False):
         """Return the next `batch_size` examples from this data set."""
         if fake_data:
-            fake_image = [1.0 for _ in xrange(784)]
+            fake_image = [1.0 for _ in range(784)]
             fake_label = 0
-            return [fake_image for _ in xrange(batch_size)], [
-                fake_label for _ in xrange(batch_size)]
+            return [fake_image for _ in range(batch_size)], [
+                fake_label for _ in range(batch_size)]
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
         if self._index_in_epoch > self._num_examples:
